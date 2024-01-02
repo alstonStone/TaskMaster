54c8e8324b9588008a94b04d873a09878bc15b58
alstonStone <alstonStone0@gmail.com>
2024-01-02 11:28:50 -0500

    Added window for creating tasks

---
+++ TaskMaster/src/main/java/AlstonStone/TaskMaster/CreateTaskDisplayController.java
@@ -0,0 +1,21 @@
+package AlstonStone.TaskMaster;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class CreateTaskDisplayController {
+
+    @FXML
+    private Button CreateTaskButton;
+
+    @FXML
+    private TextField TaskTextField;
+
+    @FXML
+    void CreateTask(ActionEvent event) {
+
+    }
+
+}
--- TaskMaster/src/main/java/AlstonStone/TaskMaster/TaskMasterHomeScreenController.java
+++ TaskMaster/src/main/java/AlstonStone/TaskMaster/TaskMasterHomeScreenController.java
@@ -2,9 +2,15 @@ package AlstonStone.TaskMaster;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class TaskMasterHomeScreenController {
 
@@ -21,7 +27,16 @@ public class TaskMasterHomeScreenController {
     private Label TitleLable;
 
     @FXML
-    void OpenAddTaskDisplay(ActionEvent event) {
+    void OpenAddTaskDisplay(ActionEvent event) throws Exception {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("CreateTaskDisplay.fxml"));
+            Parent root1 = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root1));  
+            stage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
 
     }
 
